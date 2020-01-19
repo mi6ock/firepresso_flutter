dbaa1d49e21a87518990af676c87fcc6c384f61a
mi6ock <miy606@gmail.com>
2020-01-19 15:12:57 +0900

    add

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
-import 'package:firepresso/home.dart';
 import 'package:flutter/material.dart';
+
+import 'home.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  GlobalKey key = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
--- lib/screen/article_screen.dart
+++ lib/screen/article_screen.dart
@@ -8,36 +8,16 @@ class ArticleScreen extends StatefulWidget {
 class _ArticleScreenState extends State<ArticleScreen> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Row(
       children: <Widget>[
         Expanded(
           flex: 3,
           child: Column(
             children: <Widget>[
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
-              Expanded(
-                  child: Card(
-                color: Colors.redAccent,
-              )),
+              ContainerSample(),
+              ContainerSample(),
+              ContainerSample(),
             ],
           ),
         ),
@@ -47,8 +27,75 @@ class _ArticleScreenState extends State<ArticleScreen> {
         ),
         Expanded(
           flex: 7,
-          child: Placeholder(),
+          child: SingleChildScrollView(
+            child: SizedBox(
+              width: size.width * 0.5,
+              height: size.height,
+              child: Card(
+                child: Column(
+                  children: <Widget>[
+                    Card(
+                      child: SizedBox(
+                        height: 100,
+                        width: size.width,
+                        child: Text(
+                          "text",
+                          style: Theme.of(context).textTheme.title,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
         )
+      ],
+    );
+  }
+
+  Widget ContainerSample() {
+    return Column(
+      children: <Widget>[
+        Center(
+          child: Container(
+            margin: EdgeInsets.only(top: 20),
+            width: 150, //幅
+            height: 200, //高さ
+            decoration: BoxDecoration(
+              color: Colors.greenAccent, //色
+              borderRadius: BorderRadius.all(
+                Radius.circular(10),
+              ),
+            ),
+          ),
+        ),
+        Material(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Height',
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: 'Width',
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ],
     );
   }
