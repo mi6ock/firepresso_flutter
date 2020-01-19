926baaf793e936661bee3d4698f3a0a4476a5da3
mi6ock <miy606@gmail.com>
2020-01-19 13:29:08 +0900

    add drawer

--- lib/home.dart
+++ lib/home.dart
@@ -3,6 +3,36 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            ListTile(
+              title: Text('記事追加'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+              },
+            ),
+            ListTile(
+              title: Text('About'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: SizedBox(
+        height: 3000,
+        width: 300,
+        child: Container(
+          color: Colors.redAccent,
+        ),
+      ),
+    );
   }
 }
